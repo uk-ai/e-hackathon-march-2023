1594368b2c8d6f53a78a4c1d7f1e6cb580b75d83
cabrerac <chc79@cam.ac.uk>
2023-02-21 16:25:29 +0000

    updating styles and js

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function openTab(evt, tab_name) {
+function open_tab(evt, tab_name) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -17,4 +17,40 @@ function openTab(evt, tab_name) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tab_name).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function load_file(file_path, div_id){
+  $.ajax({
+    url: file_path,
+    success: function (data){
+      var rows = data.split("\n");
+      // Create table element
+      var table = document.createElement("table");
+      var header = table.createTHead();
+      var body = table.createTBody();
+      // Fill the table
+      for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].split(",");
+        if (cells.length > 1) {
+          if (i == 0){
+            var row = header.insertRow(0);
+            for (var j = 0; j < cells.length; j++) {
+              var cell = row.insertCell(-1);
+              cell.innerHTML = "<b>" + cells[j] + "</b>";
+            }
+          } else{
+            var row = body.insertRow(-1);
+            for (var j = 0; j < cells.length; j++) {
+              var cell = row.insertCell(-1);
+              cell.innerHTML = cells[j];
+            }
+          }
+        }
+      }
+      // Add table to div
+      var dvCSV = document.getElementById(div_id);
+      dvCSV.innerHTML = "";
+      dvCSV.appendChild(table);
+    }
+  });
 }
